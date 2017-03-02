2baf1122ab826547729f8389aef456c8d20baa5c
Chris Belanger <belanger.chris@gmail.com>
2017-03-01 22:04:39 -0500

    setting up structure

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
+import {Unit} from './Models/unit'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
   title = 'app works!';
+   Units: Unit[] = [
+  { id: 1, name: 'Teaspoon'},
+  { id: 2, name: 'Pound'}
+];
+
 }
+
+
