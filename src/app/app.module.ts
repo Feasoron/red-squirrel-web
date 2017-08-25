637d9870e34a11042722b66c1f8d7c43312a557a
Chris Belanger <belanger.chris@gmail.com>
2017-08-24 22:54:59 -0400

    requires login to view

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,6 +13,7 @@ import { LoginAreaComponent } from './components/login-area.component';
 import {AuthService} from './services/auth.service';
 import { RouterModule } from '@angular/router';
 import {CallbackComponent} from './callback/callback.component';
+import {AuthorizednAreaComponent} from './components/authorized-area.component';
 
 @NgModule({
   declarations: [
@@ -22,7 +23,8 @@ import {CallbackComponent} from './callback/callback.component';
     FoodListComponent,
     HeaderComponent,
     LoginAreaComponent,
-    CallbackComponent
+    CallbackComponent,
+    AuthorizednAreaComponent
   ],
   imports: [
     BrowserModule,
